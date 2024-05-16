3fa772e47aa1876396f04af460f04a4dcf41a69f
MinhAn <voongocminhan20072002@gmail.com>
2024-05-16 11:59:51 +0700

    get count user in waitinglist

--- CoursesService/routes/courses.js
+++ CoursesService/routes/courses.js
@@ -56,5 +56,43 @@ router.get("/departments/:department", async (req, res) => {
   }
 });
 
+router.get("/departments", async (req, res) => {
+  try {
+    // Find distinct departments from courses
+    const departments = await Course.distinct("department");
+    res.status(200).json(departments);
+  } catch (error) {
+    console.error("Error getting departments:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+router.get("/waitinglist/:courseId/:semesterName", async (req, res) => {
+  try {
+    const { courseId, semesterName } = req.params;
 
+    // Find the semester based on semester name
+    const semester = await Semester.findOne({ semester_name: semesterName });
+
+    if (!semester) {
+      return res.status(404).json({ message: "Semester not found" });
+    }
+
+    // Find the course in the semester's courses array
+    const course = semester.courses.find(
+      (course) => course.course_id === courseId
+    );
+
+    if (!course) {
+      return res
+        .status(404)
+        .json({ message: "Course not found in this semester" });
+    }
+
+    // Return the waiting list for the course
+    res.json({ waitingList: course.waitingList });
+  } catch (error) {
+    console.error("Error fetching waiting list:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 module.exports = router;
